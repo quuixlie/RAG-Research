60e4185114359a23b5bada8dfe9f91b3134b7c98
Jimmy Foxx <quuixlie@gmail.com>
2025-04-18 01:20:02 +0200

    feat: Add classic rag test pipeline

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from config import Config
-from rag.rag_architectures.rag_architecture_factory import RAGArchitectureFactory
-from pymupdf import Document
+from pipelines.rag_test_pipeline import rag_test_pipeline
 
-
-document = Document("/home/quuixlie/Desktop/Pan Tadeusz.pdf", filetype="pdf")
-
-config = Config()
-classic_rag = RAGArchitectureFactory(config.rag_architecture_name, config=config)
-
-classic_rag.process_document(2137, document)
-print(classic_rag.process_query(2137, "Who was the father of Pan Tadeusz?"))
-classic_rag.get_rag_architecture().remove_conversation(2137)
+if __name__ == "__main__":
+    # Run the RAG test pipeline
+    rag_test_pipeline(
+        document_path="/home/quuixlie/Desktop/Pan Tadeusz.pdf",
+        query="Who is the father of Pan Tadeusz?",
+        conversation_id=2137
+    )
---
+++ pipelines/rag_test_pipeline.py
@@ -0,0 +1,16 @@
+from config import Config
+from rag.rag_architectures.rag_architecture_factory import RAGArchitectureFactory
+from pymupdf import Document
+
+
+def rag_test_pipeline(document_path: str, query: str, conversation_id: int) -> None:
+    document = Document(document_path, filetype="pdf")
+
+    config = Config()
+    classic_rag = RAGArchitectureFactory(config.rag_architecture_name, config=config)
+
+    classic_rag.process_document(conversation_id, document)
+
+    print(classic_rag.process_query(conversation_id, query))
+
+    classic_rag.get_rag_architecture().remove_conversation(conversation_id)
