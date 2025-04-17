4fb9634cbb89e063ab9a4d7fcbe11f254018763a
Jimmy Foxx <quuixlie@gmail.com>
2025-04-18 00:31:27 +0200

    refactor: Change the query in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from config import Config
 from rag.rag_architectures.rag_architecture_factory import RAGArchitectureFactory
 from pymupdf import Document
-import logging
 
 
 document = Document("/home/quuixlie/Desktop/Pan Tadeusz.pdf", filetype="pdf")
@@ -9,6 +8,6 @@ document = Document("/home/quuixlie/Desktop/Pan Tadeusz.pdf", filetype="pdf")
 config = Config()
 classic_rag = RAGArchitectureFactory("classic-rag", config=config)
 
-classic_rag.process_document(4, document)
-print(classic_rag.process_query(4, "Who was food poisonned?"))
-classic_rag.get_rag_architecture().remove_conversation(4)
+classic_rag.process_document(5, document)
+print(classic_rag.process_query(5, "Who was the father of Pan Tadeusz?"))
+classic_rag.get_rag_architecture().remove_conversation(5)
